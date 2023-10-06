b8f45a385cf938858c14b1aabc868e022ebe14e5
van-dat <dangvandat1d@gmail.com>
2023-10-06 22:07:10 +0700

    heart

---
+++ app.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    document.getElementById('vehicle-body').style.display = 'none';
+    document.getElementById('tree-wrap').style.display = 'none';
+    window.location.href = "heart.html";
+  }, 3000)
+  setTimeout(function() {
+    document.getElementById('ground-line').style.display = 'none';
+    document.getElementById('pinkboard').style.display = '';
+    document.getElementById('tree-wrap').style.display = 'none';
+  }, 2000)
